067ef21ac090de600d550d81ca8a9fae71b1f5f0
Dmitriy Istomin <distomin+git@pm.me>
2023-09-13 00:34:05 -0400

    update resume style, add more resume fields

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,21 @@ module.exports = {
     "./dist/**/*.html",
   ],
   theme: {
+    colors: {
+      'offwhite': '#F7FAFC',
+      'midnight': '#171923',
+      'night': '#1A202C',
+      'twilight': '#4A5568',
+      'highlight': '#C53030',
+      'lightgrey': '#E2E8F0',
+    },
     fontFamily: {
       sans: ['IBM Plex Sans', 'sans-serif'],
       display: ['IBM Plex Sans Condensed', 'sans-serif'],
     },
+    listStyleType: {
+      square: 'square',
+    }
   },
   plugins: [],
 }
